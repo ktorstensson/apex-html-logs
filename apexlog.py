d53408b256770bbcbbd70baa73fdf3b851344717
Kalle Torstensson <ktorsten@eso.org>
2018-03-20 14:31:50 -0300

    cleanup of quote signs

--- apexlog.py
+++ apexlog.py
@@ -5,7 +5,7 @@ import re
 
 
 def read_sourcecat(cat=None):
-    """Parse APEX source catalogue (.cat)
+    '''Parse APEX source catalogue (.cat)
 
     Parameters
     ----------
@@ -16,27 +16,27 @@ def read_sourcecat(cat=None):
     -------
     sci_sources : list
         Names of science sources
-    """
+    '''
 
     sci_sources = []
 
     if cat is None:
         from os.path import expanduser
         from getpass import getuser
-        cat = expanduser("~/") + getuser() + ".cat"
+        cat = expanduser('~/') + getuser() + '.cat'
 
     # print("Reading science sources from:", cat)
     with open(cat) as f:
         s = f.readlines()
 
     for line in s:
-        if ((line[0] != "!") & (line[0] != "\n")):
+        if ((line[0] != '!') & (line[0] != '\n')):
             sci_sources.append(line.split()[0])
     return sci_sources
 
 
 def read_linecat(cat=None):
-    """Parse APEX line cat .lin
+    '''Parse APEX line cat .lin
 
     Parameters
     ----------
@@ -47,21 +47,21 @@ def read_linecat(cat=None):
     -------
     sci_lines : dict
         Dictionary of science lines / frequencies
-    """
+    '''
     sci_lines = []
     sci_freqs = []
 
     if cat is None:
         from os.path import expanduser
         from getpass import getuser
-        cat = expanduser("~/") + getuser() + ".lin"
+        cat = expanduser('~/') + getuser() + '.lin'
 
     # print("Reading science lines from:", cat)
     with open(cat) as f:
         s = f.readlines()
 
     for line in s:
-        if ((line[0] != "!") & (line[0] != "\n")):
+        if ((line[0] != '!') & (line[0] != '\n')):
             sci_lines.append(line.split()[0])
             sci_freqs.append(float(line.split()[1]))
     sci_lines = dict(zip(sci_lines, sci_freqs))
@@ -69,7 +69,7 @@ def read_linecat(cat=None):
 
 
 def read_one(filename):
-    """Read a html obslog to pandas DataFrame
+    '''Read a html obslog to pandas DataFrame
 
     Parameters
     ----------
@@ -80,23 +80,23 @@ def read_one(filename):
     -------
     df : pandas.DataFrame
         The obslog data
-    """
+    '''
 
     import pandas as pd
     df = pd.read_html(filename, header=0)[0]
     # df.set_index("Scan")
     df['UTC'] = pd.to_datetime(df.UTC, format='%Y-%m-%dU%H:%M:%S')
-    df["Scan duration"] = pd.to_timedelta(df["Scan duration"], unit="s")
+    df['Scan duration'] = pd.to_timedelta(df['Scan duration'], unit='s')
     return df
 
 
 def get_line(string):
-    """Get first white spaced deliminated part of string"""
+    '''Get first white spaced deliminated part of string'''
     return string.split()[0]
 
 
 def read_obslogs(dir=None):
-    """Read APEX html obslogs
+    '''Read APEX html obslogs
 
     Parameters
     ----------
@@ -107,22 +107,22 @@ def read_obslogs(dir=None):
     -------
     df : pandas.DataFrame
         The obslog data
-    """
+    '''
 
     import pandas as pd
     from os.path import expanduser
     from glob import glob
 
     if dir is None:
-        dir = expanduser("~/obslogs/")
+        dir = expanduser('~/obslogs/')
 
-    logs = glob(dir + "*.html")
+    logs = glob(dir + '*.html')
 
     # print("Reading obslogs:", logs)
     df = read_one(logs[0])
     for log in logs[1:]:
         df = pd.concat([df, read_one(log)], axis=0)
-    df["Line"] = df["Mol. line"].apply(get_line)
+    df['Line'] = df['Mol. line'].apply(get_line)
     df.rename(columns=(lambda x: re.sub('[().]', '', x)), inplace=True)
     df.rename(columns=(lambda x: re.sub('[ -]', '_', x)), inplace=True)
     df.rename(columns=(lambda x: x.lower()), inplace=True)
@@ -133,7 +133,7 @@ def read_obslogs(dir=None):
 
 
 def summarise_sciobs(sci_sources, sci_lines, df):
-    """Summarise science observations in DataFrame
+    '''Summarise science observations in DataFrame
 
     Parameters
     ----------
@@ -148,7 +148,7 @@ def summarise_sciobs(sci_sources, sci_lines, df):
     -------
     dfs : pandas.DataFrame
         Summary of scan duration [min] for sources/lines
-    """
+    '''
 
     from numpy import timedelta64
 
@@ -174,7 +174,7 @@ def main():
     return sci_sources, sci_lines, df, dfs
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # import pandas as pd
     sci_sources, sci_lines, df, dfs = main()
     print(dfs)
